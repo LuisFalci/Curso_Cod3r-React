b53c6cab46e6b4886f244ede543074ec4389abd6
LuisFalci <78694561+LuisFalci@users.noreply.github.com>
2022-02-26 11:57:43 -0300

    Manipulação de Parâmetro

--- curso-react/src/components/basicos/ComParametro.js
+++ curso-react/src/components/basicos/ComParametro.js
@@ -1,12 +1,16 @@
 import React from 'react'
 
 export default function ComParametro(props){
-    const status = props.nota >= 7 ? 'Aprovado' : 'Recuperação' //se nota maior ou igual a 7 retorna aprovado
+    //se nota maior ou igual a 7 retorna aprovado
+    const status = props.nota >= 7 ? 'Aprovado' : 'Recuperação' 
+
+    //parâmetros não podem ser alterados (apenas leitura), mas podemos atribuíl-los a outras varíaveis e assim muda-los 
+    const notaInt = Math.ceil(props.nota)
     return(
         <div>
             <p>{props.cor}</p> 
             <p>{props.texto}</p>
-            <p>{props.nota}</p>
+            <p>{notaInt}</p>
             <p>Status: {status}</p>
         </div>
     )
