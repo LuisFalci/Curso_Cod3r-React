376f104b6803dddf969fda067495cce06f7a5cbc
LuisFalci <78694561+LuisFalci@users.noreply.github.com>
2022-02-26 23:18:02 -0300

    CSS - Flex Box

--- curso-react/src/App.js
+++ curso-react/src/App.js
@@ -4,24 +4,27 @@ import Primeiro from "./components/basicos/Primeiro";
 import ComParametro from "./components/basicos/ComParametro";
 import Aleatorio from "./components/basicos/Aleatorio";
 import Card from "./components/layout/Card";
+import "./App.css";
 
 //export default (props) => {
 
 export default function App(props) {
   return (
-    <div id="app">
+    <div className="App">
       <h1>Fundamentos React</h1>
-      <Card titulo="#03 - Desafio Aleatório">
-        <Aleatorio max={10} min={1} />
-      </Card>
- 
-      <Card titulo="#02 - Componente com Parâmetros">
-        <ComParametro aluno="Luis" nota={10.0} />
-      </Card>
+      <div className="Cards">
+        <Card titulo="#03 - Desafio Aleatório">
+          <Aleatorio max={10} min={1} />
+        </Card>
 
-      <Card titulo="#01 - Primeiro Componente">
-        <Primeiro />
-      </Card>
+        <Card titulo="#02 - Componente com Parâmetros">
+          <ComParametro aluno="Luis" nota={10.0} />
+        </Card>
+
+        <Card titulo="#01 - Primeiro Componente">
+          <Primeiro />
+        </Card>
+      </div>
     </div>
   );
 }
