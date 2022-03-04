b5768ee702a5ab3cf1b277a5bd2a44a8edb67dba
LuisFalci <78694561+LuisFalci@users.noreply.github.com>
2022-03-04 00:57:16 -0300

    Rederenzição Condicional - Componente If/Else

--- curso-react/src/components/condicional/If.js
+++ curso-react/src/components/condicional/If.js
@@ -1,7 +1,21 @@
-export default function If(props){
-    if(props.test){
-            return props.children 
-    }else{
-        return false
-    }
+export default function If(props) {
+  // Buscamos pelos filhos de If (no caso Else) por meio do filter
+  // Se chil.type estiver setado e child.type.name for igual a Else, retorna o primeiro filho. O [0] significa que queremos pegar o primeiro filho do array
+  const elseChild = props.children.filter((child) => {
+    return child.type && child.type.name === "Else";
+  })[0];
+
+  const ifChildren = props.children.filter((child) => {
+    return child !== elseChild;
+  });
+
+  if (props.test) {
+    return ifChildren;
+  } else if (elseChild) {
+    return elseChild;
+  } else {
+    return false;
+  }
 }
+
+export const Else = (props) => props.children;
--- curso-react/src/components/condicional/UsuarioInfo.js
+++ curso-react/src/components/condicional/UsuarioInfo.js
@@ -1,13 +1,17 @@
 import React from 'react'
-import If from './If'
+import If, { Else } from './If'
 
 export default function UsuarioInfo(props){
     //pega o usuario e caso não exista, ele é vazio
     const usuario = props.usuario || {}
     return(
         <div>
+            {/* If é pai de Else */}
            <If test={usuario && usuario.nome}>
            Seja bem vindo <strong> { usuario.nome}</strong>
+           <Else>
+               Seja bem vindo <strong>Amigão</strong>
+           </Else>
            </If>
         </div>
     )
