32915917804d6b2812bcff04f2f2cf810c429f6e
LuisFalci <78694561+LuisFalci@users.noreply.github.com>
2022-03-04 21:11:47 -0300

    Contador dividido em componentes + Comunicação de filho para pai

--- curso-react/src/components/contador/Contador.js
+++ curso-react/src/components/contador/Contador.js
@@ -1,59 +1,56 @@
 import React, { Component } from "react";
-import './Contador.css';
+import Botoes from "./Botoes";
+
+import "./Contador.css";
+import Display from "./Display";
+import PassoForm from "./PassoForm";
 
 class Contador extends Component {
-// Como acessar propriedades da classe: this.props
-// exemplo: this.props.numeroInicial 
-    state = {
-        numero: this.props.numeroInicial || 0,
-        passo: this.props.passoInicial || 5,
-    };
+  // Como acessar propriedades da classe: this.props
+  // exemplo: this.props.numeroInicial
+  state = {
+    numero: this.props.numeroInicial || 0,
+    passo: this.props.passoInicial || 5,
+  };
 
-    // Outra sintaxe para pegar o props:
-    // constructor(props){
-    //     super(props)
+  // Outra sintaxe para pegar o props:
+  // constructor(props){
+  //     super(props)
 
-    //     this.state = {
-    //         numero: props.numeroInicial 
-    //     }
-    // }
+  //     this.state = {
+  //         numero: props.numeroInicial
+  //     }
+  // }
 
-    // setState é uma função de Component
-    // Escrevemos a função assim: inc = () =>, pois toda vez que a função for chamada, this irá apontar para o novo estado da função
-    incremento = () => {
-        this.setState({
-            numero: this.state.numero + this.state.passo
-        })
-    }
+  // setState é uma função de Component
+  // Escrevemos a função assim: inc = () =>, pois toda vez que a função for chamada, this irá apontar para o novo estado da função
+  incremento = () => {
+    this.setState({
+      numero: this.state.numero + this.state.passo,
+    });
+  };
 
-    decremento = () => {
-        this.setState({
-            numero: this.state.numero - this.state.passo
-        })
-    }
+  decremento = () => {
+    this.setState({
+      numero: this.state.numero - this.state.passo,
+    });
+  };
 
-    setPasso = (evento) => {
-        this.setState({
-            // o retorno será uma string, colocando o + na frente a string será convertida em inteiro
-            passo: +evento.target.value,
-        })
-    }
+  //passo é a quantidade que eu quero decrementar ou incrementar ao valor inicial
+  setPasso = (novoPasso) => {
+    this.setState({
+      // o retorno será uma string, colocando o + na frente a string será convertida em inteiro
+      passo: novoPasso,
+    });
+  };
 
   render() {
     return (
       <div className="Contador">
         <h2>Contador</h2>
-        <h3>{this.state.numero}</h3>
-        <div>
-            <label htmlFor="passoInput"> Passo: </label>
-            <input
-            id="passoInput"
-            type="number"
-            value={this.state.passo}
-            onChange={this.setPasso}></input>
-        </div>
-        <button onClick={this.incremento}>+</button>
-        <button onClick={this.decremento}>-</button>
+        <Display numero={this.state.numero} />
+        <PassoForm passo={this.state.passo} setPasso={this.setPasso} />
+        <Botoes setInc={this.incremento} setDec={this.decremento}/>
       </div>
     );
   }
