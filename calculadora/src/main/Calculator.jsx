5d0b5ba1cf000e889dc0ef9bcb3b2f474f4fa88f
LuisFalci <78694561+LuisFalci@users.noreply.github.com>
2022-03-06 16:11:26 -0300

    Calculadora - Lógica Parte  1

--- calculadora/src/main/Calculator.jsx
+++ calculadora/src/main/Calculator.jsx
@@ -1,53 +1,92 @@
-import React, { Component } from 'react'
-import Button from '../components/Button'
-import Display from '../components/Display'
-import './Calculator.css'
+import React, { Component } from "react";
+import Button from "../components/Button";
+import Display from "../components/Display";
+import "./Calculator.css";
 
-
+//estado inicial da calculadora
+const initialState = {
+  // valor inicial do display
+  displayValue: "0",
+  // variável responsável por limpar o display
+  clearDisplay: false,
+  // armazenas as operações
+  operation: null,
+  // array de 2, guarda o anterior e o atual
+  values: [0, 0],
+  // variável que indica quem é o valor atual no array anterior
+  current: 0,
+};
 
 export default class Calculator extends Component {
- 
-    // com o construtor, não precisamos mais usar arrow function (=>) para referenciar o elemento atual (.this)
-    constructor(props) {
-        super(props)
-        this.clearMemory = this.clearMemory.bind(this)
-        this.setOperation = this.setOperation.bind(this)
-        this.addDigit = this.addDigit.bind(this)
-    }
-    clearMemory(){
-        console.log('limpar')
-    }
+  // clone do estado inicial
+  state = { ...initialState };
 
-    setOperation(operation){
-        console.log(operation)
-    }
+  // com o construtor, não precisamos mais usar arrow function (=>) para referenciar o elemento atual (.this)
+  constructor(props) {
+    super(props);
+    this.clearMemory = this.clearMemory.bind(this);
+    this.setOperation = this.setOperation.bind(this);
+    this.addDigit = this.addDigit.bind(this);
+  }
+  // reseta o clone do estado inicial
+  clearMemory() {
+    this.setState({ ...initialState });
+  }
 
-    addDigit(n){
-        console.log(n)
+  setOperation(operation) {
+    console.log(operation);
+  }
+
+  addDigit(n) {
+    // controle de excessão (não pode ter dois pontos, apenas um)
+    if (n === "." && this.state.displayValue.includes(".")) {
+      return;
     }
-    
-    render(){
-        return(
-            <div className='calculator'>
-                <Display value={100} />
-                <Button label="AC" click={this.clearMemory} triple />
-                <Button label="/" click={this.setOperation} operation />
-                <Button label="7" click={this.addDigit} />
-                <Button label="8" click={this.addDigit} />
-                <Button label="9" click={this.addDigit} />
-                <Button label="*" click={this.setOperation} operation />
-                <Button label="4" click={this.addDigit} />
-                <Button label="5" click={this.addDigit} />
-                <Button label="6" click={this.addDigit} />
-                <Button label="-" click={this.setOperation} operation />
-                <Button label="1" click={this.addDigit} />
-                <Button label="2" click={this.addDigit} />
-                <Button label="3" click={this.addDigit} />
-                <Button label="+" click={this.setOperation} operation />
-                <Button label="0" click={this.addDigit} double />
-                <Button label="." click={this.addDigit} />
-                <Button label="=" click={this.setOperation} operation />
-                </div>
-        )
+    // ao adicionar um número significativo, limpamos o zero caso haja (zero a esquerda)
+    const clearDisplay =
+      this.state.displayValue === "0" || this.state.clearDisplay;
+    // o valor atual é igual ao do display, porém se o display for ser limpo, o valor atual é vazio 
+    const currentValue = clearDisplay ? "" : this.state.displayValue;
+    // adiciona o valor no display
+    const displayValue = currentValue + n;
+    // salvo o estado do display, e mudo o clear para false, pois já foi limpo
+    this.setState({ displayValue, clearDisplay: false });
+
+
+    if (n !== ".") {
+      const i = this.state.current;
+      const newValue = parseFloat(displayValue);
+      const values = [...this.state.values];
+    //O valor atual (i), recebe o novo valor 
+      values[i] = newValue;
+    //Salva o estado do array de valores 
+      this.setState({ values });
+      console.log(values);
     }
+  }
+
+  render() {
+    return (
+      <div className="calculator">
+        <Display value={this.state.displayValue} />
+        <Button label="AC" click={this.clearMemory} triple />
+        <Button label="/" click={this.setOperation} operation />
+        <Button label="7" click={this.addDigit} />
+        <Button label="8" click={this.addDigit} />
+        <Button label="9" click={this.addDigit} />
+        <Button label="*" click={this.setOperation} operation />
+        <Button label="4" click={this.addDigit} />
+        <Button label="5" click={this.addDigit} />
+        <Button label="6" click={this.addDigit} />
+        <Button label="-" click={this.setOperation} operation />
+        <Button label="1" click={this.addDigit} />
+        <Button label="2" click={this.addDigit} />
+        <Button label="3" click={this.addDigit} />
+        <Button label="+" click={this.setOperation} operation />
+        <Button label="0" click={this.addDigit} double />
+        <Button label="." click={this.addDigit} />
+        <Button label="=" click={this.setOperation} operation />
+      </div>
+    );
+  }
 }
