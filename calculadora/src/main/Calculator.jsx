ef6ef561a5a32fe268e31d1856040d5146706544
LuisFalci <78694561+LuisFalci@users.noreply.github.com>
2022-03-06 16:55:16 -0300

    Calculadora - Lógica Parte 2

--- calculadora/src/main/Calculator.jsx
+++ calculadora/src/main/Calculator.jsx
@@ -34,7 +34,33 @@ export default class Calculator extends Component {
   }
 
   setOperation(operation) {
-    console.log(operation);
+    // se o valor atual é a posição 0 do array, iremos limpar o display e colocar o novo valor na segunda posição do array
+    if (this.state.current === 0) {
+      this.setState({ operation, current: 1, clearDisplay: true });
+    } 
+    // caso o array esteja com os dois números (valor atual na segunda posição do array (values[1])), iremos fazer a operação 
+    else {
+      const equals = operation === "=";
+      const currentOperation = this.state.operation;
+
+      const values = [...this.state.values];
+      try {
+        // a função eval(), permite a realização de contas passando a operação por meio de texto (sem atributos)
+        values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`);
+      } catch (e) {
+        values[0] = this.state.values[0];
+      }
+
+      values[1] = 0;
+
+      this.setState({
+        displayValue: values[0],
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: !equals,
+        values,
+      });
+    }
   }
 
   addDigit(n) {
@@ -45,23 +71,21 @@ export default class Calculator extends Component {
     // ao adicionar um número significativo, limpamos o zero caso haja (zero a esquerda)
     const clearDisplay =
       this.state.displayValue === "0" || this.state.clearDisplay;
-    // o valor atual é igual ao do display, porém se o display for ser limpo, o valor atual é vazio 
+    // o valor atual é igual ao do display, porém se o display for ser limpo, o valor atual é vazio
     const currentValue = clearDisplay ? "" : this.state.displayValue;
     // adiciona o valor no display
     const displayValue = currentValue + n;
     // salvo o estado do display, e mudo o clear para false, pois já foi limpo
     this.setState({ displayValue, clearDisplay: false });
 
-
     if (n !== ".") {
       const i = this.state.current;
       const newValue = parseFloat(displayValue);
       const values = [...this.state.values];
-    //O valor atual (i), recebe o novo valor 
+      //O valor atual (i), recebe o novo valor
       values[i] = newValue;
-    //Salva o estado do array de valores 
+      //Salva o estado do array de valores
       this.setState({ values });
-      console.log(values);
     }
   }
 
