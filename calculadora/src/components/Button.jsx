c32b766fad584188984ff8d08fd1789a7b4db329
LuisFalci <78694561+LuisFalci@users.noreply.github.com>
2022-03-06 12:25:44 -0300

    Corpo Calculadora

--- calculadora/src/components/Button.jsx
+++ calculadora/src/components/Button.jsx
@@ -1,16 +1,19 @@
-import React from "react";
-import "./Button.css";
+import React from 'react'
+import './Button.css'
 
-export default (props) => {
-    let classes = 'button'
+export default props => {
+    let classes = 'button '
     classes += props.operation ? 'operation' : ''
     classes += props.double ? 'double' : ''
     classes += props.triple ? 'triple' : ''
 
     return(
         // o botão aplica a className que a ele for passada
-        <button className={classes}>
-            {props.label}   
-        </button>
-   )
-}   
+        // ao clicarmos no botão, será gerado um evento (e) que com a arrowFunction (=>) irá receber o evento click() com o parâmetro da label do botão 
+        <button 
+        onClick={e => props.click && props.click(props.label)}
+        className={classes}>
+        {props.label}
+    </button>
+)
+}
