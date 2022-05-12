ad2993b3aea14b98f5dea6900e218cb8f514e475
LuisFalci <78694561+LuisFalci@users.noreply.github.com>
2022-05-12 18:21:55 -0300

    Response e Request Dinâmicos

--- fundamentos/fundamentos/src/pages/api/hello.js
+++ fundamentos/fundamentos/src/pages/api/hello.js
@@ -1,5 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+// http://localhost:3000/api/hello?nome=luis&idade=2
 export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe", method: req.method });
+  res
+    .status(200)
+    .json({
+      name: "John Doe",
+      metodo: req.method,
+      nome: req.query.nome,
+      idade: +req.query.idade,
+    });
 }
