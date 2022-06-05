3e13b0b3c1f1dfd989997b1e1684cb91525e3fd7
LuisFalci <78694561+LuisFalci@users.noreply.github.com>
2022-06-05 10:08:15 -0300

    Componente Layout e Titulo

--- aplicacao-crud/next-crud/src/pages/index.tsx
+++ aplicacao-crud/next-crud/src/pages/index.tsx
@@ -1,14 +1,18 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Layout from '../components/Layout'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={
       `flex h-screen justify-center items-center
-      bg-gradient-to-r from-purple-500`
+      bg-gradient-to-r from-purple-500 to-blue-500
+      text-white`
     }>
-     <h1>Olá Mundo</h1>
+     <Layout titulo="Cadastro Simples">
+       <span>Filho de Layout</span>
+     </Layout>
     </div>
   )
 }
